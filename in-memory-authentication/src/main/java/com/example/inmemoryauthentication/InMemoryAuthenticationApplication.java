bc081ba24e186cc9afc587036bd972c73713e567
thainguyen101b <thainguyen101b@gmail.com>
2025-06-15 22:31:14 +0700

    in memory authentication

---
+++ in-memory-authentication/src/main/java/com/example/inmemoryauthentication/InMemoryAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.example.inmemoryauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMemoryAuthenticationApplication.class, args);
+    }
+
+
+}
